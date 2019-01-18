a83e0cdc31166bf54733c4652e6a8151fd0130d0
Dana Bennett <danabennett10@gmail.com>
2019-01-18 13:51:08 -0500

    Add some thoughts on the score page

--- src/views/Scores.vue
+++ src/views/Scores.vue
@@ -1,8 +1,26 @@
 <template>
   <section class="section">
     <div class="container">
+      <progress class="progress is-danger" value="10" max="100">15%</progress>
       <h1 class="title">Scores</h1>
       <h2 class="subtitle">Great score content was going here...</h2>
+      <div class="main-content">
+        <h2>Scores Page</h2>
+        <ul>
+          <li>Live Updates, including score, round.</li>
+          <li>Match Previews for games that haven't started, including rosters, predictions</li>
+          <li>Match Recaps for games that haven concluded</li>
+        </ul>
+        <h2>Match Pages</h2>
+        <ul>
+          <li>Live Updates from all four boards</li>
+          <li>View all rounds, including match ups of later rounds, results of previous rounds</li>
+          <li>Match Recaps for games that haven concluded</li>
+          <li>
+            <a href="/nba-chart.png">NBA style "Match Flow" Chart</a>
+          </li>
+        </ul>
+      </div>
     </div>
   </section>
 </template>
@@ -15,3 +33,17 @@ export default {
   name: "Scores"
 };
 </script>
+
+<style scoped>
+.main-content h2 {
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+ul {
+  list-style-type: disc;
+}
+li {
+  margin: 1em;
+}
+</style>
+
